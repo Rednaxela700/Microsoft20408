4d2016b38d0e3a5c83cad28172ade3bd488d675f
Alexander <ohleg@windowslive.com>
2017-12-03 09:47:19 +0100

    did a todo task

--- html/2/labmod05/ContosoConf/scripts/pages/schedule.js
+++ html/2/labmod05/ContosoConf/scripts/pages/schedule.js
@@ -8,6 +8,24 @@ var track2CheckBox = document.getElementById("show-track-2");
 //       The response will be a JSON object of the form "{ schedule: [ ... ] }"
 //       Save the array into the schedule variable
 //       Then call displaySchedule()
+function downloadSchedule() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "/schedule/list", true);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            try {
+            var response = JSON.parse(request.responseText);
+            if (request.status === 200) {
+                schedule = response.schedule;
+                displaySchedule();
+            } else {
+                alert(response.message);
+            }
+        } catch (exception) {
+            alert("Schedule list not selected");
+        }
+    }
+    };
 
 function createSessionElement(session) {
     var li = document.createElement("li");
@@ -67,6 +85,7 @@ function handleListClick(event) {
             saveStar(listItem.sessionId, true);
         }
     }
+}
 }
 
 track1CheckBox.addEventListener("click", displaySchedule, false);
